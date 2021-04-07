bca9d5fa048fe5ef75004565b5ab571c2d602230
raghu-singh-dataselect <leo_raghu@hotmail.com>
2021-04-07 13:43:55 +0100

    Updated the salary for User:Raghu

--- src/main/java/com/hmrc/hrsystem/dao/UserDAO.java
+++ src/main/java/com/hmrc/hrsystem/dao/UserDAO.java
@@ -14,7 +14,7 @@ public class UserDAO {
     private static List<User> users = new ArrayList<>();
 
     public static List<User> findAll() {
-        users.add(new User(101l, "Raghu", "Singh", 44, new BigDecimal(40000), Gender.Male));
+        users.add(new User(101l, "Raghu", "Singh", 44, new BigDecimal(50000), Gender.Male));
         users.add(new User(102l, "Adarsh", "Brar", 42, new BigDecimal(55000), Gender.Male));
         users.add(new User(103l, "Harry", "Singh", 42, new BigDecimal(65000), Gender.Male));
         users.add(new User(104l, "Bitu", "Singh", 48, new BigDecimal(75000), Gender.Male));
